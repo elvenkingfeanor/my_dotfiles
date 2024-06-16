9af3a35328134f371968611952a97d9bc630df7c
elvenkingfeanor <seniorsarkar.de@gmail.com>
2024-06-16 12:55:42 +0530

    refinements

--- nvim/lua/packer-config/init.lua
+++
@@ -1,4 +0,0 @@
-return require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
-    use {'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = { {'nvim-lua/plenary.nvim'} }}
-end)
